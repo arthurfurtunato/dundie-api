b974f6a4f703b9c86280814c47dcc256b01c11c9
arthurfurtunato <arthurfurtunato@gmail.com>
2024-09-09 10:25:45 -0300

    Adição de duas novas rotas na tag user

--- dundie/routes/user.py
+++ dundie/routes/user.py
@@ -1,8 +1,9 @@
 from sqlmodel import Session, select
 from dundie.db import ActiveSession
-from dundie.models.user import User, UserResponse
+from dundie.models.user import User, UserResponse, UserRequest
 from fastapi import APIRouter
 from typing import List
+from http import HTTPStatus as http
 
 router = APIRouter()
 
@@ -12,3 +13,23 @@ async def list_users(*, session: Session = ActiveSession):
 
   users = session.exec(select(User)).all()
   return users
+
+@router.get("/{username}", response_model=UserResponse)
+async def get_user_by_username(*, session: Session = ActiveSession, username: str):
+  """Get single user by username"""
+
+  query = select(User).where(User.username == username)
+  user = session.exec(query).first()
+
+  return user
+
+@router.post("/", response_model=UserResponse, status_code=http.CREATED)
+async def create_user(*, session: Session = ActiveSession, user: UserRequest):
+  """Create a new user"""
+
+  db_user = User.from_orm(user)
+  session.add(db_user)
+  session.commit()
+  session.refresh(db_user)
+
+  return db_user
