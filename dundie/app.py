542ae2457e6d17bbdb8d3013926da2de756d2d17
arthurfurtunato <arthurfurtunato@gmail.com>
2024-09-15 15:41:59 -0300

    JWT Tokens e auth configs

--- dundie/app.py
+++ dundie/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from .routes import main_router
 
 app = FastAPI(
---
+++ dundie/auth.py
@@ -0,0 +1,97 @@
+from datetime import timedelta, datetime
+from typing import Optional, Callable, Union
+from fastapi import HTTPException, Request, status
+from jose import JWTError, jwt
+from pydantic import BaseModel
+from dundie.config import settings
+from functools import partial
+from fastapi.security import OAuth2PasswordBearer
+
+from dundie.models.user import User
+from dundie.security import verify_password
+from dundie.db import engine
+from sqlmodel import Session, select
+
+ALGORITHM = settings.security.ALGORITHM
+SECRET_KEY = settings.security.SECRET_KEY
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Models
+
+class Token(BaseModel):
+  access_token: str
+  token_type: str
+  refresh_token: str
+
+class RefreshToken(BaseModel):
+  refresh_token: str
+
+class TokenData(BaseModel):
+  username: Optional[str] = None
+
+# Funcions
+
+def create_access_token(
+  data: dict,
+  expires_delta: Optional[timedelta] = None,
+  scope: str = "access_token"
+) -> str:
+  """Create a JWT Token"""
+
+  to_encode = data.copy()
+  expires_delta = expires_delta or timedelta(minutes=15)
+  expire = datetime.now() + expires_delta
+  to_encode.update({"exp": expire, "scope": scope})
+
+  encoded_jwt = jwt.encode(
+    to_encode,
+    SECRET_KEY,
+    ALGORITHM
+  )
+
+  return encoded_jwt
+
+create_refresh_token = partial(create_access_token, scope="refresh_token")
+
+def authenticate_user(get_user: Callable, username: str, password: str) -> Union[User, bool]:
+  """verifies the user exists and password is correct"""
+  user = get_user(username)
+
+  if not user:
+    return False
+
+  if not verify_password(password, user.password):
+    return False
+  
+  return user
+
+def get_user(username: str) -> Optional[User]:
+  """Get a user by username"""
+  # TODO - Mover para um módulo de utilidades
+  query = select(User).where(User.username == username)
+  with Session(engine) as session:
+    return session.exec(query).first()
+  
+def get_current_user(token: str):
+  """Get the current user from a token"""
+  creadential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+
+  try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    username = payload.get("sub")
+    if username is None:
+      raise creadential_exception
+    token_data = TokenData(username=username)
+  except JWTError:
+    raise creadential_exception
+  
+  user = get_user(username=token_data.username)
+  if user is None:
+    raise creadential_exception
+  
+  return user
+
+def validate_token(token: str):
+  user = get_current_user(token=token)
+  return user
